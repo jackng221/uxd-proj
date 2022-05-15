c7c57f2101cfe61c59130b35dad86b676b58ae44
jackng221 <jackng1233@gmail.com>
2022-05-16 03:21:30 +0800

    Update NavBarSub.js

--- src/components/NavBarSub.js
+++ src/components/NavBarSub.js
@@ -1,21 +1,29 @@
-import { AppBar, Button, ButtonGroup, MenuItem, Popper, Toolbar } from '@mui/material'
+import { AppBar, Button, Collapse, Divider, List, ListItemButton, Toolbar } from '@mui/material'
 import React from 'react'
 import styled from '@emotion/styled';
 import { theme } from '../theme';
+import { Box } from '@mui/system';
 
 const StyledToolbar = styled(Toolbar)({
     justifyContent: "space-between",
 })
-const StyledButton = styled(Button)({
-    color: theme.palette.text.main,
+// const StyledButton = styled(Button)({
+//     color: theme.palette.text.main,
+//     fontSize: 20,
+//     padding: 0,
+//     width: "200px",
+//     height: "50px",
+//     backgroundColor: theme.palette.secondary.main,
+//     '&:hover': {
+//         backgroundColor: theme.palette.secondary.dark,
+//     },
+// })
+const StyledListButton = styled(ListItemButton)({
     fontSize: 20,
     padding: 0,
-    width: "200px",
+    width: "180px",
     height: "50px",
-    backgroundColor: theme.palette.secondary.main,
-    '&:hover': {
-        backgroundColor: theme.palette.secondary.dark,
-    },
+    justifyContent: "center",
 })
 const StyledCurrentPos = styled("div")({
     backgroundColor: "white",
@@ -25,44 +33,67 @@ const StyledCurrentPos = styled("div")({
 })
 
 function NavBarSub() {
-    const [open, setOpen] = React.useState(false);
-    const anchorRef = React.useRef(null);
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
+    const [open, setOpen] = React.useState("");
 
-    const HandleParentClick = () => {
-        //onClick = {}
-    };
-    const HandleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-    }
-    const handleClose = (event) => {
-        if (anchorRef.current && anchorRef.current.contains(event.target)) {
-            return;
+    const handleClickCategories = () => {
+        switch(open){
+            case "cat": setOpen(""); break;
+            default: setOpen("cat"); break;
         }
-
-        setOpen(false);
     };
+    const handleClickFAQ = () => {
+        switch(open){
+            case "faq": setOpen(""); break;
+            default: setOpen("faq"); break;
+        }
+    };
+
     return (
         <React.Fragment>
             <AppBar position="static" sx={{
                 backgroundColor: theme.palette.secondary.main,
             }}>
                 <StyledToolbar variant="dense">
-                    <div>
-                        <ButtonGroup>
-                            <StyledButton>Categories</StyledButton>
-                        </ButtonGroup>
-                        <StyledButton>FAQ</StyledButton>
-                    </div>
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                    }}>
+                        <List sx={{
+                            padding:0
+                        }}>
+                            <StyledListButton onClick={handleClickCategories}>Item Categories</StyledListButton>
+                            <Collapse in={open=="cat"} timeout="auto" unmountOnExit>
+                                <List>
+                                    <StyledListButton>Types</StyledListButton><Divider/>
+                                    <StyledListButton>Brand</StyledListButton><Divider/>
+                                    <StyledListButton>Region</StyledListButton><Divider/>
+                                    <StyledListButton>Free shipping</StyledListButton><Divider/>
+                                    <StyledListButton>Samples</StyledListButton><Divider/>
+                                    <StyledListButton>Clearance</StyledListButton>
+                                </List>
+                            </Collapse>
+                        </List>
+                        <List sx={{
+                            padding:0
+                        }}>
+                            <StyledListButton onClick={handleClickFAQ}>FAQ</StyledListButton>
+                            <Collapse in={open=="faq"} timeout="auto" unmountOnExit>
+                                <List>
+                                    <StyledListButton>Shipping</StyledListButton><Divider/>
+                                    <StyledListButton>Payment</StyledListButton><Divider/>
+                                    <StyledListButton>Privacy notice</StyledListButton><Divider/>
+                                    <StyledListButton>Gift certificate</StyledListButton><Divider/>
+                                    <StyledListButton>Site map</StyledListButton><Divider/>
+                                    <StyledListButton>Contact us</StyledListButton>
+                                </List>
+                            </Collapse>
+                        </List>
+                    </Box>
                     <StyledCurrentPos>
                         You are at:
                     </StyledCurrentPos>
                 </StyledToolbar>
             </AppBar>
-            <ButtonGroup orientation='vertical' style={{paddingLeft: "24px"}}>
-                <StyledButton>A</StyledButton>
-                <StyledButton>B</StyledButton>
-            </ButtonGroup>
         </React.Fragment>
     )
 }
